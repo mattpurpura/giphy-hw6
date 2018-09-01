7f5779c64fd51e8b052cc79d3388ff50a4bb4dcc
mattpurpura <mpurpura212@gmail.com>
2018-08-31 17:50:53 -0700

    API works, site needs structure

---
+++ main.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+var apiKey = "2VrCH1KQe29aD0h3YALA3m4VmgK9hVyQ";
+
+
+
+
+// var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag="+searchTerm;
+
+
+$("#submit").on("click", function(event){
+    event.preventDefault();
+    var btn = $('<button>').attr("value", $("#newButton").val()).text($("#newButton").val());
+    btn.addClass("gif-maker");
+    $("#buttons-go-here").append(btn);
+})
+
+$(document).on("click", ".gif-maker", function(){
+    var searchTerm = $(this).val();
+    console.log(searchTerm);
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&limit=10&q="+searchTerm;
+   
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        console.log(response)
+        var data = response.data;
+        for (let i=0; i< data.length; i++){
+            var div = $("<div class='gif-div'>");
+            var img = $("<img src>");
+            var stillURL = data[i].images.fixed_height_still.url;
+            var animateURL = data[i].images.fixed_height.url;
+            img.attr("src", stillURL).attr("data-still", stillURL).attr("data-animate", animateURL);
+            div.append(img);
+            $("#gifs-go-here").append(div);
+        }
+    });
+})
+
+$(document).on("click", "img", function(){
+    if ($(this).attr("src") === $(this).attr("data-still")){
+        $(this).attr("src", $(this).attr("data-animate"));
+    }
+    else{
+        $(this).attr("src", $(this).attr("data-still"));
+    }
+
+
+})
+
+
+})//ends document ready function
